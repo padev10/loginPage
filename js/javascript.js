83f6d7a85b9dec5a13dddb39f83ca924bf7c0bed
padev10 <papaselfemployed@gmail.com>
2023-10-05 11:44:37 +0100

    jsFolderCommented

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+let eyeBtn = document.getElementById("eye");
+let clicked = 1;
+
+eyeBtn.addEventListener("click", function () {
+  if (clicked) {
+    // chow password
+    document.getElementById("input2").type = "text";
+    // change eye image
+    document.getElementById("eye").className = "fa fa-eye";
+    clicked = 0;
+  } else {
+    // hide password
+    document.getElementById("input2").type = "password";
+    // change eye image
+    let a = document.getElementById("eye");
+    a.classList.remove("fa-eye");
+    a.classList.add("fa-eye-slash");
+    //back to initial condition
+    clicked = 1;
+  }
+});
